a9c02aa49294aa40aa918816a521d7c4f40fab37
Caglow <caglow@users.sourceforge.net>
2014-08-19 17:22:25 -0600

    Sky chart widget can now draw a series of line segments representing a path from a list of points.

--- telescope_control/chart.py
+++ telescope_control/chart.py
@@ -13,7 +13,6 @@ class Chart (glcanvas.GLCanvas):
         glcanvas.GLCanvas.__init__(self, parent, -1)
         self.context = glcanvas.GLContext(self)
         self.SetCurrent(self.context)
-        self.Bind(wx.EVT_SIZE, self.on_resize)
         
         # drawing settings
         self.path = [] # list of points [x, y]
@@ -21,31 +20,44 @@ class Chart (glcanvas.GLCanvas):
         self.h_fov = 20 # horizontal field of view
         
         # initialize OpenGL
-        width, height = self.GetSize()
-        self.resize(width, height)
         glDisable(GL_DEPTH_TEST)
+        glClearColor(0, 0, 0, 1) # black background
+        
+        # event handlers
+        self.Bind(wx.EVT_SIZE, self.on_resize)
+        self.Bind(wx.EVT_PAINT, self.on_paint)
     
     # event handlers
     def on_resize (self, event):
-        width, height = event.GetSize()
-        self.resize(width, height)
+        self.SetCurrent(self.context)
+        size = self.GetClientSize()
+        self.resize(size.width, size.height)
+        
+        self.Update()
+        self.Refresh()
+        event.Skip()
     
     def on_paint (self, event):
         wx.PaintDC(self)
         self.SetCurrent(self.context)
         self.draw()
+        self.SwapBuffers()
+        
+        event.Skip()
         
     # resize: update 2D OpenGL display with new size
     #   width, height: new chart size (pixels)
     def resize (self, width, height):
         self.width = width
         self.height = height
+        glViewport(0, 0, width, height)
         
         # set up view so top-left is (0, 0)
         glMatrixMode(GL_PROJECTION)
         glLoadIdentity()
         glOrtho(0, width, height, 0, 0, 1)
         glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
     
     # project: convert from sky coordinates to screen coordinates
     #          using equirectangular projection
@@ -68,8 +80,21 @@ class Chart (glcanvas.GLCanvas):
         
         # convert displacement from center in sky coordinates into displacement
         # in screen coordinates and compute final screen position
-        return [0.5 * self.width  + displace[0] * pix_per_deg,
-                0.5 * self.height - displace[1] * pix_per_deg]
+        return 0.5 * self.width  + displace[0] * pix_per_deg, \
+               0.5 * self.height - displace[1] * pix_per_deg
     
     # draw: draw all objects onto the screen
-    
+    def draw (self):
+        self.resize(self.width, self.height)
+        glClear(GL_COLOR_BUFFER_BIT) # clear previous drawing
+        
+        # draw white lines representing the path
+        glColor(1, 1, 1)
+        glBegin(GL_LINES)
+        
+        for point in self.path:
+            screen_x, screen_y = self.project(point)
+            glVertex(screen_x, screen_y)
+        
+        glEnd()
+        glFlush()
