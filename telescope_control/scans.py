f6ef008c0c0a9652478ef802cc248d3b532f3fcf
Caglow <caglow@users.sourceforge.net>
2014-08-23 12:32:16 -0600

    Adjusted scans to work properly over the poles.

--- telescope_control/scans.py
+++ telescope_control/scans.py
@@ -4,6 +4,8 @@
 # note: crd_a = az, ra, lon, etc.
 #       crd_b = el, de, lat, etc.
 
+import circle
+
 # serpentine: go from left to right, go up half a step, and go back left and repeat
 #
 #   pt1, pt2, pt3, pt4 -> [crd_a, crd_b]: corners forming the scan boundary
@@ -14,27 +16,25 @@ def serpentine (pt1, pt2, pt3, pt4, num_turns):
     
     crd_list = [] # final list of coordinates
     
+    # find bearings and distance from pt1 -> pt3 and pt2 -> pt4
+    bearing_14 = circle.bearing(pt1, pt4)
+    dist_14 = circle.distance(pt1, pt4)
+    bearing_23 = circle.bearing(pt2, pt3)
+    dist_23 = circle.distance(pt2, pt3)
+    
     for i in range(0, num_turns):
         
         # start on pt1 side and move toward pt2 side
-        crd_list.append(
-            [(1 - float(i)/num_turns)*pt1[0] + float(i)/num_turns*pt4[0],
-             (1 - float(i)/num_turns)*pt1[1] + float(i)/num_turns*pt4[1]]
-        )
-        crd_list.append(
-            [(1 - float(i)/num_turns)*pt2[0] + float(i)/num_turns*pt3[0],
-             (1 - float(i)/num_turns)*pt2[1] + float(i)/num_turns*pt3[1]]
-        )
+        crd_list.append(circle.waypoint(pt1, bearing_14,
+            dist_14 * float(i) / num_turns))
+        crd_list.append(circle.waypoint(pt2, bearing_23,
+            dist_23 * float(i) / num_turns))
         
         # move towards pt3 half a step and go the other direction
-        crd_list.append(
-            [(1 - (i + 0.5)/num_turns)*pt2[0] + (i + 0.5)/num_turns*pt3[0],
-             (1 - (i + 0.5)/num_turns)*pt2[1] + (i + 0.5)/num_turns*pt3[1]]
-        )
-        crd_list.append(
-            [(1 - (i + 0.5)/num_turns)*pt1[0] + (i + 0.5)/num_turns*pt4[0],
-             (1 - (i + 0.5)/num_turns)*pt1[1] + (i + 0.5)/num_turns*pt4[1]]
-        )
+        crd_list.append(circle.waypoint(pt2, bearing_23,
+            dist_23 * (i + 0.5) / num_turns))
+        crd_list.append(circle.waypoint(pt1, bearing_14,
+            dist_14 * (i + 0.5) / num_turns))
     
     # execute the final stretch of the scan
     crd_list.append([pt4[0], pt4[1]])
@@ -53,17 +53,19 @@ def zigzag (pt1, pt2, pt3, pt4, num_turns):
     
     crd_list = [] # final list of coordinates
     
+    # find bearings and distance from pt1 -> pt3 and pt2 -> pt4
+    bearing_14 = circle.bearing(pt1, pt4)
+    dist_14 = circle.distance(pt1, pt4)
+    bearing_23 = circle.bearing(pt2, pt3)
+    dist_23 = circle.distance(pt2, pt3)
+    
     for i in range(0, num_turns + 1):
         
         # start on pt1 side and move toward pt2 side
-        crd_list.append(
-            [(1 - float(i)/num_turns)*pt1[0] + float(i)/num_turns*pt4[0],
-             (1 - float(i)/num_turns)*pt1[1] + float(i)/num_turns*pt4[1]]
-        )
-        crd_list.append(
-            [(1 - float(i)/num_turns)*pt2[0] + float(i)/num_turns*pt3[0],
-             (1 - float(i)/num_turns)*pt2[1] + float(i)/num_turns*pt3[1]]
-        )
+        crd_list.append(circle.waypoint(pt1, bearing_14,
+            dist_14 * float(i) / num_turns))
+        crd_list.append(circle.waypoint(pt2, bearing_23,
+            dist_23 * float(i) / num_turns))
     
     return crd_list
 
